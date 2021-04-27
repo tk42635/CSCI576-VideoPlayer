2c38b440b5f05b8570208e797f8e4c3851ac2de7
tk42635 <dehuo@usc.edu>
2021-04-26 19:05:00 -0700

    initial commit

--- VideoPlayer.java
+++ VideoPlayer.java
@@ -3,8 +3,10 @@ import java.io.FileNotFoundException;
 
 public class VideoPlayer {
     public static void main(String[] args) {
-        if(args.length < 3)
+        if(args.length < 3) {
             System.err.println("usage: java VideoPlayer [VideoFileDirectory(.rgb)] [AudioFile]");
+            return;
+        }
 		String audiofilename = args[1];
 		// opens the inputStream
 		FileInputStream inputStream;
