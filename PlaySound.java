51dfe2a283a615f0c1241e179304f3486463c8ee
Linjun Ouyang <lynjunoyang@gmail.com>
2021-04-29 23:24:41 -0700

    Implemented frame difference calculation

--- PlaySound.java
+++ PlaySound.java
@@ -18,88 +18,66 @@ import javax.sound.sampled.DataLine.Info;
  * 
  * @author Giulio
  */
-public class PlaySound extends Thread{
+public class PlaySound extends Thread {
 
-    private InputStream waveStream;
+	private InputStream waveStream;
 
-    private final int EXTERNAL_BUFFER_SIZE = 524288; // 128Kb
+	private final int EXTERNAL_BUFFER_SIZE = 524288; // 128Kb
 
 	private SourceDataLine dataLine;
 	private AudioInputStream audioInputStream;
 
-    /**
-     * CONSTRUCTOR
-     */
-    public PlaySound(InputStream waveStream) {
-	this.waveStream = waveStream;
-	dataLine = null;
-	audioInputStream = null;
-    }
-
-    public void play() throws PlayWaveException {
-
-	try {
-	    //audioInputStream = AudioSystem.getAudioInputStream(this.waveStream);
-		
-		//add buffer for mark/reset support, modified by Jian
-		InputStream bufferedIn = new BufferedInputStream(this.waveStream);
-	    audioInputStream = AudioSystem.getAudioInputStream(bufferedIn);
-		
-	} catch (UnsupportedAudioFileException e1) {
-	    throw new PlayWaveException(e1);
-	} catch (IOException e1) {
-	    throw new PlayWaveException(e1);
+	/**
+	 * CONSTRUCTOR
+	 */
+	public PlaySound(InputStream waveStream) {
+		this.waveStream = waveStream;
+		dataLine = null;
+		audioInputStream = null;
 	}
 
-	// Obtain the information about the AudioInputStream
-	AudioFormat audioFormat = audioInputStream.getFormat();
-	Info info = new Info(SourceDataLine.class, audioFormat);
+	public void play() throws PlayWaveException {
 
-	// opens the audio channel
-	try {
-	    dataLine = (SourceDataLine) AudioSystem.getLine(info);
-	    dataLine.open(audioFormat, this.EXTERNAL_BUFFER_SIZE);
-	} catch (LineUnavailableException e1) {
-	    throw new PlayWaveException(e1);
+		try {
+			// audioInputStream = AudioSystem.getAudioInputStream(this.waveStream);
+
+			// add buffer for mark/reset support, modified by Jian
+			InputStream bufferedIn = new BufferedInputStream(this.waveStream);
+			audioInputStream = AudioSystem.getAudioInputStream(bufferedIn);
+
+		} catch (UnsupportedAudioFileException e1) {
+			throw new PlayWaveException(e1);
+		} catch (IOException e1) {
+			throw new PlayWaveException(e1);
+		}
+
+		// Obtain the information about the AudioInputStream
+		AudioFormat audioFormat = audioInputStream.getFormat();
+		Info info = new Info(SourceDataLine.class, audioFormat);
+
+		// opens the audio channel
+		try {
+			dataLine = (SourceDataLine) AudioSystem.getLine(info);
+			dataLine.open(audioFormat, this.EXTERNAL_BUFFER_SIZE);
+		} catch (LineUnavailableException e1) {
+			throw new PlayWaveException(e1);
+		}
+
+		// Starts the music :P
+		dataLine.start();
 	}
 
-	// Starts the music :P
-	dataLine.start();
-
-	// int readBytes = 0;
-	// byte[] audioBuffer = new byte[this.EXTERNAL_BUFFER_SIZE];
-
-	// try {
-	//     while (readBytes != -1) {
-	// 	readBytes = audioInputStream.read(audioBuffer, 0,
-	// 		audioBuffer.length);
-	// 	if (readBytes >= 0){
-	// 	    dataLine.write(audioBuffer, 0, readBytes);
-	// 	}
-	//     }
-	// } catch (IOException e1) {
-	//     throw new PlayWaveException(e1);
-	// } finally {
-	//     // plays what's left and and closes the audioChannel
-	//     dataLine.drain();
-	//     dataLine.close();
-	// }
-
-    }
-
-	public void run()
-    {
-        try {
-            int readBytes = 0;
+	public void run() {
+		try {
+			int readBytes = 0;
 			byte[] audioBuffer = new byte[this.EXTERNAL_BUFFER_SIZE];
 
 			try {
 				while (readBytes != -1) {
-				readBytes = audioInputStream.read(audioBuffer, 0,
-					audioBuffer.length);
-				if (readBytes >= 0){
-					dataLine.write(audioBuffer, 0, readBytes);
-				}
+					readBytes = audioInputStream.read(audioBuffer, 0, audioBuffer.length);
+					if (readBytes >= 0) {
+						int written = dataLine.write(audioBuffer, 0, readBytes);
+					}
 				}
 			} catch (IOException e1) {
 				throw new PlayWaveException(e1);
@@ -107,11 +85,10 @@ public class PlaySound extends Thread{
 				// plays what's left and and closes the audioChannel
 				dataLine.drain();
 				dataLine.close();
+			}
+		} catch (Exception e) {
+			// Throwing an exception
+			System.out.println("Exception is caught");
+		}
 	}
-        }
-        catch (Exception e) {
-            // Throwing an exception
-            System.out.println("Exception is caught");
-        }
-    }
 }
--- VideoPlayer.java
+++ VideoPlayer.java
@@ -2,17 +2,17 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class VideoPlayer {
-    public static void main(String[] args) {
-        if(args.length < 2) {
-            System.err.println("usage: java VideoPlayer [VideoFileDirectory(.rgb)] [AudioFile]");
-            return;
-        }
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.err.println("usage: java VideoPlayer [VideoFileDirectory(.rgb)] [AudioFile]");
+			return;
+		}
 		String audiofilename = args[1];
 		// opens the inputStream
 		FileInputStream inputStream;
 		try {
 			inputStream = new FileInputStream(audiofilename);
-			//inputStream = this.getClass().getResourceAsStream(filename);
+			// inputStream = this.getClass().getResourceAsStream(filename);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			return;
@@ -26,12 +26,13 @@ public class VideoPlayer {
 			e.printStackTrace();
 			return;
 		}
-		
-		ImageDisplay ren = new ImageDisplay();
-		ren.showIms(args[0]);
 
-        ren.start();
-        playSound.start();
-		
+		ImageDisplay ren = new ImageDisplay();
+		ren.initialize(args[0]);
+		ren.detectShots();
+
+		//ren.start();
+		//playSound.start();
+
 	}
 }
